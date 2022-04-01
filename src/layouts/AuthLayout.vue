d43c64b909b061cc57b1d3771c0d88cf000b038b
naymin <konayminhtwe@gmail.com>
2022-04-01 15:19:46 +0630

    Login Page Template

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,6 +1,10 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-page-container>
+      <div class="auth-heading q-my-lg">
+        <div class="text-h3 text-primary">PropTech</div>
+        <div class="text-h6 text-brown col-mt-sm">EdTech School Management</div>
+      </div>
       <router-view />
     </q-page-container>
   </q-layout>
@@ -13,3 +17,9 @@ export default defineComponent({
   name: 'MainLayout'
 })
 </script>
+
+<style scoped>
+.auth-heading {
+  text-align: center;
+}
+</style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,11 +1,66 @@
 <template>
-  <q-page class="flex flex-center"> </q-page>
+  <q-page class="q-mt-xl">
+    <div class="row justify-center q-py-lg">
+      <div class="text-h3">Log In</div>
+    </div>
+    <div class="row justify-center">
+      <q-input
+        standout="bg-teal text-white"
+        v-model="email"
+        label="Email"
+        class="q-pa-md col-8 col-lg-4"
+      />
+    </div>
+    <div class="row justify-center">
+      <q-input
+        standout="bg-teal text-white"
+        v-model="password"
+        label="Password"
+        class="q-pa-md col-8 col-lg-4"
+      />
+    </div>
+    <q-space />
+    <div class="row justify-center q-pt-xl">
+      <q-btn
+        unelevated
+        rounded
+        color="red"
+        label="Log In"
+        class="col-4 col-lg-1"
+      />
+    </div>
+    <div class="q-pt-xl">
+      <div class="row justify-center items-center">
+        <div class="text-body2">
+          Don't Have PropTech Account?
+        </div>
+      </div>
+      <div class="row justify-center items-center">
+        <q-btn
+          flat
+          rounded
+          color="primary"
+          label="Register"
+          class="col-4 col-lg-1"
+        />
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'LoginPage'
+  name: 'LoginPage',
+  setup () {
+    const email = ref('')
+    const password = ref('')
+
+    return {
+      email,
+      password
+    }
+  }
 })
 </script>
